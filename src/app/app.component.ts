d8ba8a404b2695d72a1063979f2e02549e8115b9
Adrian Machado <machadoadriandev@gmail.com>
2025-10-13 19:44:37 -0300

    pages about

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from "./shared/footer/footer.component";
 import { PortfolioComponent } from "./pages/portfolio/portfolio.component";
+import { AboutComponent } from "./pages/about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, PortfolioComponent],
+  imports: [HeaderComponent, FooterComponent, PortfolioComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
